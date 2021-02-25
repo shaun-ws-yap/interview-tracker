084df8f753d0337fec6fadfa22d0be75dc0986ac
Shaun Yap <me@shaunyap.ca>
2021-02-24 17:07:06 -0800

    Add Hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from './components/NavBar/NavBar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
